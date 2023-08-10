d8d624f1824bd68a2a7c048a156e04d9bf364a73
Ljusilj <129436912+Ljusilj@users.noreply.github.com>
2023-08-10 18:19:22 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,75 @@
+import VimeoPlayer  from '@vimeo/player';
+import throttle from 'lodash.throttle';
+console.log("player", VimeoPlayer)
 
+
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+
+player.on('timeupdate', throttle(onTime, 1000));
+
+function onTime(data) {
+    let currentTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", currentTime);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    if (savedTime !== null) {
+        player.setCurrentTime(parseFloat(savedTime))
+            .then(function(seconds) {
+               
+            })
+            .catch(function(error) {
+                switch (error.name) {
+                    case 'RangeError':
+                        
+                        break;
+
+                    default:
+                        
+                        break;
+                }
+            });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    
+    
+    
+
+    
+
+
+
+    
